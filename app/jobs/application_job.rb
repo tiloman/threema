ebe597ea89136c2f03b85d3c56117e68da3fc7a1
Timo Lohmann <lohmanntimo@gmail.com>
2020-07-20 15:51:25 +0200

    added application job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,12 @@
 class ApplicationJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(*args)
+    SyncGroupsJob.perform_later
+    SyncMembersJob.perform_later
+    SyncGroupImagesJob.perform_later
+
+    #schedule new jobs every x hours due to lack of cronjobs in dokku
+    ApplicationJob.set(wait: 3.hour).perform_later
+  end
 end
