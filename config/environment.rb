2e7ef6324385148c4d708a42fbbefc25d974f6ae
Timo Lohmann <lohmanntimo@gmail.com>
2020-07-25 10:44:30 +0200

    updated sendgrid params

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,9 @@ local_env = File.join(Rails.root, 'config', 'local_env.rb')
 load(local_env) if File.exists?(local_env)
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_API_KEY'],
-  :password => ENV['SENDGRID_API_KEY'],
-  :domain => 'timolohmann.de',
+  :user_name  => ENV['SENDGRID_USERNAME'],
+  :password   => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
