0281ad7a7693665ec73fb96bde66f30408f3030a
Timo Lohmann <lohmanntimo@gmail.com>
2020-08-05 12:13:03 +0200

    add all checkbox

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -79,36 +79,53 @@ function addMemberToSelect(new_member, button) {
 
 //alle Checkboxen selektieren
 
-function selectAll(table,checkbox){
-let selectAllBox = document.getElementById(checkbox);
-let usersTable = document.getElementById(table);
-let checkboxes = usersTable.querySelectorAll('input[type=checkbox]');
+function selectAll(checkbox){
+let selectAllBox = document.getElementById("allUsersBox");
+let usersTable = document.getElementById("usersTable");
+let checkboxes = usersTable.getElementsByClassName('user_checkbox');
 let searchInput = document.getElementById('search').value;
 let num = checkboxes.length;
 
 
   if (selectAllBox.checked == true) {
     //wenn etwas eingegeben wurde in das Suchfeld
-    if (searchInput != '' && checkbox == 'allUsersBox') {
+    if (searchInput != '') {
         let value = searchInput.toLowerCase();
 
         $("#usersTable tr").filter(function() {
+
            if(this.style.display != 'none') {
-             this.childNodes[0].childNodes[0].checked = true;
+             //this.childNodes[0].childNodes[0].checked = true;
+             addMemberToSelect(this.children[0].children[0].id, this.children[0].children[0])
            }
         });
 
     } else {
     //alle Felder werden selektiert
+    if (num > 300) {
+      alert("Mehr als 300 Personen ausgewählt. Das kann ein bisschen dauern...")
+    }
     for (var i=0; i<num; i++) {
-        checkboxes[i].checked = true;
+        //checkboxes[i].checked = true;
+        addMemberToSelect(checkboxes[i].id, checkboxes[i])
       }
     }
 
   } else {
   //keins wird selektiert
+  if ($('#group_member_ids').length) {
+    var select2_form = $('#group_member_ids');
+  }
+  else if ($('#distribution_list_member_ids').length) {
+    var select2_form = $('#distribution_list_member_ids');
+  }
+  var selected_now = select2_form.val();
+  var members = [];
+  select2_form.val(members); // Select the option with a value of '1'
+  select2_form.trigger('change'); // Notify any JS components that the value changed
     for (var i=0; i<num; i++) {
-      checkboxes[i].checked = false;
+      //checkboxes[i].checked = false;
+      remove_add_btn_from_table(checkboxes[i].id)
        }
   }
 
@@ -118,18 +135,6 @@ let num = checkboxes.length;
 function add_me(user) {
   let button = document.getElementById(user);
   addMemberToSelect(user, button)
-}
-
-//Auswahl löschen
-function unSelectAll(){
-  let usersTable = document.getElementById('usersTable');
-  let checkboxes = usersTable.querySelectorAll('input[type=checkbox]');
-  let num = checkboxes.length;
-
-  for (let i=0; i<num; i++) {
-    checkboxes[i].checked = false;
-
-     }
 }
 
 
@@ -145,3 +150,59 @@ $(document).on('turbolinks:load', function () {
 
 
 })
+
+
+// function sortUsersTable(n) {
+//   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+//   table = document.getElementById("usersTable");
+//   switching = true;
+//   // Set the sorting direction to ascending:
+//   dir = "asc";
+//   /* Make a loop that will continue until
+//   no switching has been done: */
+//   while (switching) {
+//     // Start by saying: no switching is done:
+//     switching = false;
+//     rows = table.rows;
+//     /* Loop through all table rows (except the
+//     first, which contains table headers): */
+//     for (i = 1; i < (rows.length - 1); i++) {
+//       // Start by saying there should be no switching:
+//       shouldSwitch = false;
+//       /* Get the two elements you want to compare,
+//       one from current row and one from the next: */
+//       x = rows[i].getElementsByTagName("TD")[n];
+//       y = rows[i + 1].getElementsByTagName("TD")[n];
+//       /* Check if the two rows should switch place,
+//       based on the direction, asc or desc: */
+//       if (dir == "asc") {
+//         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+//           // If so, mark as a switch and break the loop:
+//           shouldSwitch = true;
+//           break;
+//         }
+//       } else if (dir == "desc") {
+//         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+//           // If so, mark as a switch and break the loop:
+//           shouldSwitch = true;
+//           break;
+//         }
+//       }
+//     }
+//     if (shouldSwitch) {
+//       /* If a switch has been marked, make the switch
+//       and mark that a switch has been done: */
+//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//       switching = true;
+//       // Each time a switch is done, increase this count by 1:
+//       switchcount ++;
+//     } else {
+//       /* If no switching has been done AND the direction is "asc",
+//       set the direction to "desc" and run the while loop again. */
+//       if (switchcount == 0 && dir == "asc") {
+//         dir = "desc";
+//         switching = true;
+//       }
+//     }
+//   }
+// }
